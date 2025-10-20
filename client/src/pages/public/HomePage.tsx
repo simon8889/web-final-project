d57fdba8f2b5af2dabeca1a544d882a2ebfaead6
simon <simondjv2016@gmail.com>
2025-10-20 00:42:32 -0500

    Signin page done

--- client/src/pages/public/HomePage.tsx
+++ client/src/pages/public/HomePage.tsx
@@ -1,13 +1,30 @@
 import Hero from '@/components/ui/Hero'
 import Pricing from '@/components/ui/Pricing'
 import Contact from '@/components/ui/Contact'
+import { BubbleBackground } from '@/components/ui/shadcn-io/bubble-background'
+
+const colorConfig = {
+  first: '113,113,122', // zinc-500
+  second: '161,161,170', // zinc-400
+  third: '212,212,216', // zinc-300
+  fourth: '63,63,70', // zinc-700
+  fifth: '39,39,42', // zinc-800
+  sixth: '24,24,27', // zinc-900
+}
 
 const HomePage = () => {
   return (
-    <div>
-      <Hero />
-      <Pricing />
-      <Contact />
+    <div className="relative">
+      <BubbleBackground
+        interactive={true}
+        colors={colorConfig}
+        className="fixed inset-0 z-0 bg-black"
+      />
+      <div className="relative z-200">
+        <Hero />
+        <Pricing />
+        <Contact />
+      </div>
     </div>
   )
 }
--- client/src/pages/public/SignInPage.tsx
+++ client/src/pages/public/SignInPage.tsx
@@ -1,5 +1,13 @@
-const SignIn = () => {
-  return <div>SignIn</div>
+import SignInForm from '@/components/SignInForm'
+
+const SignupPage = () => {
+  return (
+    <div className="bg-black flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <SignInForm />
+      </div>
+    </div>
+  )
 }
 
-export default SignIn
+export default SignupPage
